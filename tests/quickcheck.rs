7d804c5380c0f6e0a82c992f7e2e594b892860ff
Mateusz Cegiełka <mateusz@cegla.net>
2020-11-02 19:36:49 +0100

    :white_check_mark: Test insert and pop with std BinaryHeap

--- tests/quickcheck.rs
+++ tests/quickcheck.rs
@@ -1,5 +1,20 @@
 use fibheap::FibHeap;
 use quickcheck_macros::quickcheck;
+use std::cmp::Ordering;
+
+#[derive(Eq, PartialEq)]
+struct Reverse<T>(T);
+
+impl<T: PartialOrd> PartialOrd for Reverse<T> {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		other.0.partial_cmp(&self.0)
+	}
+}
+impl<T: Ord+PartialOrd> Ord for Reverse<T> {
+	fn cmp(&self, other: &Self) -> Ordering {
+		other.0.cmp(&self.0)
+	}
+}
 
 #[quickcheck]
 fn as_many_popped_as_inserted(xs: Vec<i32>) {
@@ -12,4 +27,18 @@ fn as_many_popped_as_inserted(xs: Vec<i32>) {
 		count += 1;
 	}
 	assert_eq!(count, xs.len());
+}
+
+#[quickcheck]
+fn good_order(xs: Vec<i32>) {
+	let mut h1 = FibHeap::new();
+	let mut h2 = std::collections::BinaryHeap::new();
+	for x in xs {
+		h1.insert(x);
+		h2.push(Reverse(x));
+	}
+	while let Some(x) = h2.pop() {
+		assert_eq!(h1.pop_min(), Some(x.0));
+	}
+	assert_eq!(h1.pop_min(), None);
 }
